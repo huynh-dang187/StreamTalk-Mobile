03ad06f85f96bd308815b4a104ae4cd24e8f24ff
huynh-dang187 <112720090+huynh-dang187@users.noreply.github.com>
2025-12-23 22:49:48 +0700

    feat : setUp  enviroment ( nodejs )

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+// Cấu hình Socket.io
+const io = new Server(server, {
+    maxHttpBufferSize: 55 * 1024 * 1024 // Chuẩn bị sẵn cho file 50MB
+});
+
+// Phục vụ file html tĩnh trong thư mục public
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Có người kết nối: ' + socket.id);
+
+    // Lắng nghe sự kiện chat
+    socket.on('chat_message', (msg) => {
+        console.log('Tin nhắn nhận được: ' + msg);
+        // Gửi lại cho tất cả mọi người
+        io.emit('chat_message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User đã thoát: ' + socket.id);
+    });
+});
+
+// Chạy server ở cổng 3000
+server.listen(3000, () => {
+    console.log('Server đang chạy tại http://localhost:3000');
+});
